33c0fde575ed5925553dbc40e2c16e0ae9268248
lkdvos <lukas.devos@ugent.be>
2024-04-17 12:06:51 +0000

    Add tests for rrules with backend specified

--- test/ad.jl
+++ test/ad.jl
@@ -1,4 +1,5 @@
 using TensorOperations
+using TensorOperations: StridedBLAS, StridedNative
 using Test
 using ChainRulesTestUtils
 
@@ -19,7 +20,12 @@ precision(::Type{<:Union{Float64,Complex{Float64}}}) = 1e-8
     β = rand(T)
     A = rand(T₁, (2, 3, 4, 2, 5))
     C = rand(T₂, size.(Ref(A), pC[1]))
+
     test_rrule(tensortrace!, C, pC, A, pA, :N, α, β; atol, rtol)
+    test_rrule(tensortrace!, C, pC, A, pA, :C, α, β; atol, rtol)
+
+    test_rrule(tensortrace!, C, pC, A, pA, :C, α, β, StridedBLAS(); atol, rtol)
+    test_rrule(tensortrace!, C, pC, A, pA, :N, α, β, StridedNative(); atol, rtol)
 end
 
 @testset "tensoradd! ($T₁, $T₂)" for (T₁, T₂) in ((Float64, Float64), (Float32, Float64),
@@ -35,6 +41,9 @@ end
     β = rand(T)
     test_rrule(tensoradd!, C, pC, A, :N, α, β; atol, rtol)
     test_rrule(tensoradd!, C, pC, A, :C, α, β; atol, rtol)
+
+    test_rrule(tensoradd!, C, pC, A, :N, α, β, StridedBLAS(); atol, rtol)
+    test_rrule(tensoradd!, C, pC, A, :C, α, β, StridedNative(); atol, rtol)
 end
 
 @testset "tensorcontract! ($T₁, $T₂)" for (T₁, T₂) in
@@ -58,6 +67,11 @@ end
     test_rrule(tensorcontract!, C, pC, A, pA, :C, B, pB, :N, α, β; atol, rtol)
     test_rrule(tensorcontract!, C, pC, A, pA, :N, B, pB, :C, α, β; atol, rtol)
     test_rrule(tensorcontract!, C, pC, A, pA, :C, B, pB, :C, α, β; atol, rtol)
+
+    test_rrule(tensorcontract!, C, pC, A, pA, :N, B, pB, :N, α, β, StridedBLAS();
+               atol, rtol)
+    test_rrule(tensorcontract!, C, pC, A, pA, :C, B, pB, :N, α, β, StridedNative();
+               atol, rtol)
 end
 
 @testset "tensorscalar ($T)" for T in (Float32, Float64, ComplexF64)
