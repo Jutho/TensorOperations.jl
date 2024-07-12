ff89d7067ca7106642a71057ad58a579869d02b6
Jutho <jutho.haegeman@ugent.be>
2024-07-12 12:21:34 +0200

    fix and test zero-dimensional bumper allocations

--- test/butensor.jl
+++ test/butensor.jl
@@ -20,6 +20,8 @@ using Bumper
         HRAA1[a, s1, s2, c] := ρₗ[a, a'] * A1[a', t1, b] * A2[b, t2, c'] *
                                ρᵣ[c', c] *
                                H[s1, s2, t1, t2]
+        E1 = ρₗ[a', a] * A1[a, s, b] * A2[b, s', c] * ρᵣ[c, c'] * H[t, t', s, s'] *
+             conj(A1[a', t, b']) * conj(A2[b', t', c'])
     end
 
     # butensor implementation
@@ -27,18 +29,26 @@ using Bumper
         HRAA2[a, s1, s2, c] := ρₗ[a, a'] * A1[a', t1, b] * A2[b, t2, c'] *
                                ρᵣ[c', c] *
                                H[s1, s2, t1, t2]
+        E2 = ρₗ[a', a] * A1[a, s, b] * A2[b, s', c] * ρᵣ[c, c'] * H[t, t', s, s'] *
+             conj(A1[a', t, b']) * conj(A2[b', t', c'])
     end
     @test HRAA2 isa Array{T,4}
+    @test E2 isa T
     @test HRAA2 ≈ HRAA1
+    @test E2 ≈ E1
 
     # manual equivalent
     @no_escape @tensor allocator = default_buffer() begin
         HRAA3[a, s1, s2, c] := ρₗ[a, a'] * A1[a', t1, b] * A2[b, t2, c'] *
                                ρᵣ[c', c] *
                                H[s1, s2, t1, t2]
+        E3 = ρₗ[a', a] * A1[a, s, b] * A2[b, s', c] * ρᵣ[c, c'] * H[t, t', s, s'] *
+             conj(A1[a', t, b']) * conj(A2[b', t', c'])
     end
     @test HRAA3 isa Array{T,4}
+    @test E3 isa T
     @test HRAA3 ≈ HRAA1
+    @test E3 ≈ E1
 
     # new buffer / completely manual
     slabsize = typeof(default_buffer()).parameters[1] >> 1 # take slab size half as big
@@ -49,12 +59,16 @@ using Bumper
             HRAA4[a, s1, s2, c] := ρₗ[a, a'] * A1[a', t1, b] * A2[b, t2, c'] *
                                    ρᵣ[c', c] *
                                    H[s1, s2, t1, t2]
+            E4 = ρₗ[a', a] * A1[a, s, b] * A2[b, s', c] * ρᵣ[c, c'] * H[t, t', s, s'] *
+                 conj(A1[a', t, b']) * conj(A2[b', t', c'])
         end
         bufferlength = slabsize * length(slabbuf.slabs)
         Bumper.checkpoint_restore!(cp)
     end
     @test HRAA4 isa Array{T,4}
+    @test E4 isa T
     @test HRAA4 ≈ HRAA1
+    @test E4 ≈ E1
 
     # allocbuffer
     allocbuf = AllocBuffer(bufferlength)
@@ -62,7 +76,11 @@ using Bumper
         HRAA5[a, s1, s2, c] := ρₗ[a, a'] * A1[a', t1, b] * A2[b, t2, c'] *
                                ρᵣ[c', c] *
                                H[s1, s2, t1, t2]
+        E5 = ρₗ[a', a] * A1[a, s, b] * A2[b, s', c] * ρᵣ[c, c'] * H[t, t', s, s'] *
+             conj(A1[a', t, b']) * conj(A2[b', t', c'])
     end
     @test HRAA5 isa Array{T,4}
+    @test E5 isa T
     @test HRAA5 ≈ HRAA1
+    @test E5 ≈ E1
 end
