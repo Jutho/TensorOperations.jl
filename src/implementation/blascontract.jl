cb56c5d9dfae9a3d5abe034fdeb0fd5d677819a5
Lukas Devos <ldevos98@gmail.com>
2025-03-04 10:39:45 -0500

    Rework Strided wrapping

--- src/implementation/blascontract.jl
+++ src/implementation/blascontract.jl
@@ -47,13 +47,18 @@ function _blas_contract!(C, A, pA, B, pB, pAB, α, β, backend, allocator)
     flagC = isblasdestination(C, ipAB)
     if flagC
         C_ = C
-        _unsafe_blas_contract!(C_, A_, pA, B_, pB, ipAB, α, β)
+        _unsafe_blas_contract!(wrap_stridedview(C_),
+                               wrap_stridedview(A_), pA,
+                               wrap_stridedview(B_), pB,
+                               ipAB, α, β)
     else
-        C_ = SV(tensoralloc_add(TC, C, ipAB, false, Val(true), allocator))
-        _unsafe_blas_contract!(C_, A_, pA, B_, pB, trivialpermutation(ipAB),
-                               one(TC), zero(TC))
+        C_ = tensoralloc_add(TC, C, ipAB, false, Val(true), allocator)
+        _unsafe_blas_contract!(wrap_stridedview(C_),
+                               wrap_stridedview(A_), pA,
+                               wrap_stridedview(B_), pB,
+                               trivialpermutation(ipAB), one(TC), zero(TC))
         tensoradd!(C, C_, pAB, false, α, β, backend, allocator)
-        tensorfree!(C_.parent, allocator)
+        tensorfree!(C_, allocator)
     end
     flagA || tensorfree!(A_.parent, allocator)
     flagB || tensorfree!(B_.parent, allocator)
@@ -85,8 +90,7 @@ end
     flagA = isblascontractable(A, pA) && eltype(A) == TC
     if !flagA
         A_ = tensoralloc_add(TC, A, pA, false, Val(true), allocator)
-        Anew = SV(A_, size(A_), strides(A_), 0, A.op)
-        Anew = tensoradd!(Anew, A, pA, false, One(), Zero(), backend, allocator)
+        Anew = tensoradd!(A_, A, pA, false, One(), Zero(), backend, allocator)
         pAnew = trivialpermutation(pA)
     else
         Anew = A
